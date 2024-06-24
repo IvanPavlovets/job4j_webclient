24bcd430493172e13f5636038c28da74a53d69fb
IvanPavlovets <ivan150287@gmail.com>
2024-06-24 14:22:56 +0700

    WebClient [#505057]  сервис openfeign (В сервисном слое с помощью технологии OpenFeign будем передовать и принимать данные от сервиса source):  - rest-контроллер;  - service слой;  Для реализации OpenFeign требуеться:  1) в классе запуска @EnableFeignClient(basePackages = "ru.job4j.feign.service")  2) в сервис слое @FeignClient(name = "tracker", url = "${source-api-url}")  3) в application.properties   # Port   server.port = 8082   source-api-url =http://localhost:8080/api/item

---
+++ services/openfeign/src/main/java/ru/job4j/feign/FeignApplication.java
@@ -0,0 +1,18 @@
+package ru.job4j.feign;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@Slf4j
+@SpringBootApplication
+@EnableFeignClients(basePackages = "ru.job4j.feign.service")
+public class FeignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeignApplication.class, args);
+		log.info("Go to -> :{}", "http://localhost:8080");
+	}
+
+}
